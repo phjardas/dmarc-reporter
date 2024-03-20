841d7d0fbb6b55ff7ad00b3fcd5a475b2d561579
Philipp Jardas <philipp@jardas.de>
2024-03-20 16:34:45 +0100

    Update notifications to include total result

--- src/lambdas/sendNotification.ts
+++ src/lambdas/sendNotification.ts
@@ -16,16 +16,48 @@ export async function handler(event: DynamoDBStreamEvent) {
 async function sendNotification(records: Array<Record<string, unknown>>) {
   console.log("Sending notification for %d records", records.length);
 
-  const text = JSON.stringify(records, null, 2);
+  const results = records
+    .map(getResult)
+    .reduce(
+      (a, b) => ({ ...a, [b]: (a[b] ?? 0) + 1 }),
+      {} as Record<Result, number>
+    );
+
+  const overallResult: Result = results.fail
+    ? "fail"
+    : results.pass
+      ? "pass"
+      : "unknown";
+
+  const text = `
+Evaluated ${records.length} records:
+- Pass: ${results.pass ?? 0}
+- Fail: ${results.fail ?? 0}
+- Unknown: ${results.unknown ?? 0}
+
+${JSON.stringify(records, null, 2)}
+`.trim();
 
   await getSNS().send(
     new PublishCommand({
       TopicArn: getTopicArn(),
-      Subject: "DMARC Report",
+      Subject: `DMARC Report: ${overallResult}`,
       MessageStructure: "json",
       Message: JSON.stringify({ default: text }),
     })
   );
+}
+
+type Result = "pass" | "fail" | "unknown";
+
+function getResult(record: Record<string, unknown>): Result {
+  const { policyEvaluated } = record;
+  if (!policyEvaluated || typeof policyEvaluated !== "object") return "unknown";
+
+  const { dkim, spf } = policyEvaluated as Record<string, unknown>;
+  if (dkim === "pass" && spf === "pass") return "pass";
+  if (dkim === "fail" || spf === "fail") return "fail";
+  return "unknown";
 }
 
 const getSNS = memoize(() => new SNSClient({}));
